b61538cf3fb4c9e7112ccd17799e09baabdaf843
woshizhourui <zhouri_121@163.com>
2020-06-05 18:56:13 +0800

    Initial commit

---
+++ app/src/main/java/com/example/page/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.page;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ViewPager viewPager = findViewById(R.id.viewpager);
+        MyPageAdapter pageAdapter = new MyPageAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(pageAdapter);
+
+        TabLayout tablayout = (TabLayout) findViewById(R. id.sliding_tabs);
+        tablayout.setupWithViewPager(viewPager);
+
+
+
+    }
+}
